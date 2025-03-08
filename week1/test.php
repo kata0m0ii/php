16364c3d203d2420b1995ff8d68da28d3f186bc7
kata0m0ii <164913848+kata0m0ii@users.noreply.github.com>
2025-03-08 17:01:42 +0800

    Add files via upload

---
+++ week1/test.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = "我的第一個 PHP 網頁";
+?>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo $pageTitle; ?></title>
+</head>
+
+<body>
+  <h1><?php echo $pageTitle; ?></h1>
+</body>
+
+</html>
